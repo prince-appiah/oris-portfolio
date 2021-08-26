0ac7c81c67c7752e26354e14146305287dc48a4b
Prince Appiah <pappiah00@gmail.com>
2021-08-26 17:05:33 +0000

    Added intro section

--- src/App.js
+++ src/App.js
@@ -1,40 +1,16 @@
+import { Container } from '@chakra-ui/layout';
+import { ChakraProvider, theme } from '@chakra-ui/react';
 import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import Intro from './components/Intro';
+import { header } from './data.json';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <Container maxW="container.lg">
+        {/* Main section - with intro and avatar */}
+        <Intro header={header} />
+      </Container>
     </ChakraProvider>
   );
 }
---
+++ src/components/Intro.jsx
@@ -0,0 +1,47 @@
+import { Circle, Flex, Heading, HStack, Spacer, Text } from '@chakra-ui/layout';
+import { Image } from '@chakra-ui/image';
+import { Box, Avatar } from '@chakra-ui/react';
+
+import avatar from '../assets/avatar.png';
+
+function Intro({ header }) {
+  const { title, subtitle } = header;
+  return (
+    <Box mt="8" display="flex" justifyContent="space-between">
+      <Box display="flex" flexDirection="column" justifyContent="space-evenly">
+        <Heading
+          as="h3"
+          size="lg"
+          color="gray.800"
+          letterSpacing="wide"
+          fontWeight="bold"
+        >
+          {title}
+        </Heading>
+
+        <Text
+          fontSize="2xl"
+          lineHeight="tall"
+          letterSpacing="wide"
+          fontWeight="normal"
+          color="gray.500"
+        >
+          {subtitle}
+        </Text>
+      </Box>
+      <Box>
+        <Circle size="2xs" rounded="full">
+          <Image
+            borderRadius="full"
+            objectFit="contain"
+            bg="gray.400"
+            src={avatar}
+            alt={title}
+          />
+        </Circle>
+      </Box>
+    </Box>
+  );
+}
+
+export default Intro;
